2b90de9969c74cfea7da8d6d87cf09629cc795f1
Jaaarl <Jarl13.work@gmail.com>
2024-12-26 21:25:18 +0800

    Add: basic TypeScript examples: variables, interfaces, functions, and types

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,31 @@
 "use strict";
-let age = 20;
+let age = 25;
+const isActive = true;
+let names = ['Alice', 'Bob', 'Charlie'];
+let user = ['Alice', 30];
+const user1 = {
+    name: 'John Doe',
+    age: 28,
+    isActive: true
+};
+function greetUser(user) {
+    return `Hello, ${user.name}! You are ${user.age} years old.`;
+}
+const greetingMessage = greetUser(user1);
+function add(x, y) {
+    return x + y;
+}
+function greet(name, age) {
+    if (age) {
+        return `Hello, ${name}. You are ${age} years old.`;
+    }
+    else {
+        return `Hello, ${name}.`;
+    }
+}
+function greetWithDefault(name, age = 25) {
+    return `Hello, ${name}. You are ${age} years old.`;
+}
+let id = 12345;
+let someData = "This can be anything";
+const point = { x: 10, y: 20 };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,52 @@
-let age: number = 20;
+let age: number = 25;
+
+const isActive: boolean = true;
+
+
+let names: string[] = ['Alice', 'Bob', 'Charlie'];
+
+let user: [string, number] = ['Alice', 30];
+
+interface User {
+    name: string;
+    age: number;
+    isActive: boolean;
+}
+
+const user1: User = {
+    name: 'John Doe',
+    age: 28,
+    isActive: true
+};
+
+function greetUser(user: User): string {
+    return `Hello, ${user.name}! You are ${user.age} years old.`;
+}
+
+const greetingMessage = greetUser(user1);
+
+function add(x: number, y: number): number {
+    return x + y;
+}
+
+function greet(name: string, age?: number): string {
+    if (age) {
+        return `Hello, ${name}. You are ${age} years old.`;
+    } else {
+        return `Hello, ${name}.`;
+    }
+}
+
+function greetWithDefault(name: string, age: number = 25): string {
+    return `Hello, ${name}. You are ${age} years old.`;
+}
+
+let id: string | number = 12345;
+
+let someData: any = "This can be anything";
+type Coordinates = {
+    x: number;
+    y: number;
+};
+
+const point: Coordinates = { x: 10, y: 20 };
